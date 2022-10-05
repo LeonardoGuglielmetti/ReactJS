c6ece9678115f38845b6bcdbc7bb20b40260d94f
Leonardo Guglielmetti <106181182+LeonardoGuglielmetti@users.noreply.github.com>
2022-10-05 14:26:14 -0300

    Mejora de proyecto
    Solución de errores y categorías

--- src/components/ItemList/Cart.jsx
+++ src/components/ItemList/Cart.jsx
@@ -48,7 +48,7 @@ const Cart = () => {
                   <p className="card-text">{`Total $${item.price * quantity
                     } | `}</p>
                 </div>
-                <button onClick={()=>removeItem(item)}>Eliminar</button>
+                <button onClick={()=>removeItem(item.id)}>Eliminar</button>
               </div>
             </>
           ))}
